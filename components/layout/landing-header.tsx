3c50727e7d96417e06754e3e0ab11d5c464307e0
Dinann <notxurd46@gmail.com>
2025-04-17 00:36:35 +0700

    refactor(header): add theme toggle to landing header

--- components/layout/landing-header.tsx
+++ components/layout/landing-header.tsx
@@ -13,13 +13,14 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { Button } from "../ui/button";
 import UserAvatar from "../user-avatar";
+import { ThemeToggle } from "../theme-toggle";
 
 function LandingHeader() {
   const pathname = usePathname();
   const session = useSession();
 
   return (
-    <header className="bg-background sticky top-0 z-[99] hidden items-center justify-between border p-6 shadow-md max-md:px-4 md:flex md:px-[80px] lg:px-[144px]">
+    <header className="bg-background sticky top-0 hidden items-center justify-between border p-6 shadow-md max-md:px-4 md:flex md:px-[80px] lg:px-[144px]">
       {/* Logo */}
       <Link href="/" className="flex items-center gap-2 font-semibold">
         <div className="bg-primary text-primary-foreground flex h-6 w-6 items-center justify-center rounded-md">
@@ -42,23 +43,30 @@ function LandingHeader() {
       </div>
 
       {/* User Avatar & Sign In */}
-      {session.status === "unauthenticated" ? (
-        <div className="flex items-center gap-3">
-          <div className="flex items-center gap-4 text-sm font-medium lg:text-base">
-            <Link
-              href={"/sign-in"}
-              className="duration-200 hover:text-slate-800 hover:underline"
-            >
-              Sign In
-            </Link>
-            <Link href={"/register"}>
-              <Button className="font-medium">Register</Button>
-            </Link>
+      <div className="flex items-center gap-2">
+        <ThemeToggle />
+        {session.status === "unauthenticated" ? (
+          <div className="flex items-center gap-3">
+            <div className="flex items-center gap-4 text-sm font-medium lg:text-base">
+              <Link
+                href={"/sign-in"}
+                className="duration-200 hover:text-slate-800 hover:underline"
+              >
+                Sign In
+              </Link>
+              <Link href={"/register"}>
+                <Button className="font-medium">Register</Button>
+              </Link>
+            </div>
           </div>
-        </div>
-      ) : (
-        <UserAvatar fullname="Andre Edyson" role="SUPER_ADMIN" />
-      )}
+        ) : (
+          <UserAvatar
+            fullname="Andre Edyson"
+            role="SUPER_ADMIN"
+            email="andre@mail.com"
+          />
+        )}
+      </div>
     </header>
   );
 }
