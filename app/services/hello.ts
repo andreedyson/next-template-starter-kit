4012bcdda773acbbfd7e2572639f813def8ec05c
Dinann <notxurd46@gmail.com>
2025-07-21 08:45:37 +0700

    feat(app): add services folder for storing business logic functions

---
+++ app/services/hello.ts
@@ -0,0 +1,9 @@
+/**
+ * ⚠️ This file is a dummy service function.
+ * You can store your business logic here and separate them
+ * into different files according to your actual project needs.
+ */
+
+export async function greetUser(name: string) {
+  return `Hello! ${name}`;
+}
