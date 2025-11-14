78a7a238cb4cb1483b0f0b0d5b404ce435f15568
Dinann <notxurd46@gmail.com>
2025-11-14 21:50:55 +0700

    refactor(middleware): streamline authentication logic and remove unused token handling

--- middleware.ts
+++ middleware.ts
@@ -1,56 +1,48 @@
-import { getToken } from "next-auth/jwt";
-import type { NextRequest } from "next/server";
-import { NextResponse } from "next/server";
+import { IUserLogin } from "@/lib/profile";
+import { UserRole } from "@/types/auth";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function middleware(req: NextRequest) {
   const { pathname } = req.nextUrl;
 
-  // Special case for UploadThing endpoint
-  if (pathname.startsWith("/api/uploadthing")) {
+  const cookie = req.cookies.get("profile")?.value;
+
+  // Public Routes
+  if (pathname === "/" || pathname.startsWith("/login")) {
     return NextResponse.next();
   }
 
-  try {
-    const token = await getToken({
-      req,
-      secret: process.env.NEXTAUTH_SECRET,
-    });
-
-    // If no token found, user is unauthorized
-    if (!token) {
-      // For API routes, return JSON response
-      if (pathname.startsWith("/api/")) {
-        return NextResponse.json(
-          { error: "Unauthorized access" },
-          { status: 401 },
-        );
-      }
-
-      // For page routes, redirect to login form
-      const url = new URL("/sign-in", req.url);
-      return NextResponse.redirect(url);
-    }
-
-    return NextResponse.next();
-  } catch (error) {
-    console.error(error);
-    if (pathname.startsWith("/api/")) {
-      return NextResponse.json(
-        { error: "Authentication error" },
-        { status: 403 },
-      );
-    }
-
-    // Redirect to error page for non-API routes
-    return NextResponse.redirect(new URL("/", req.url));
+  // Redirect to login page if noo cookies
+  if (!cookie) {
+    return NextResponse.redirect(new URL("/login", req.url));
   }
+
+  const cookieValue = JSON.parse(cookie) as IUserLogin;
+
+  const roleRoutes: Record<UserRole, string> = {
+    ADMIN: "/dashboard/admin",
+    USER: "/dashboard/user",
+  };
+
+  // Prevent other role from accessing other user dashboard view
+  const userRole = cookieValue.role;
+
+  if (pathname === "/dashboard") {
+    const target = roleRoutes[userRole];
+    return NextResponse.redirect(new URL(target, req.url));
+  }
+  if (pathname.startsWith("/dashboard")) {
+    if (
+      (pathname.startsWith("/dashboard/admin") && userRole !== "ADMIN") ||
+      (pathname.startsWith("/dashboard/User") && userRole !== "USER")
+    ) {
+      return NextResponse.redirect(new URL(roleRoutes[userRole], req.url));
+    }
+  }
+
+  return NextResponse.next();
 }
 
 export const config = {
-  matcher: [
-    // Protected pages
-    "/dashboard/:path*",
-
-    // Protected API routes
-  ],
+  matcher: ["/dashboard/:path*"],
 };
