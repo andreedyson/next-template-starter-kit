eb49a3655247085f98860ed6d992303114117980
Dinann <notxurd46@gmail.com>
2025-11-14 21:49:37 +0700

    feat(dashboard): add main admin and user dashboard pages

---
+++ app/(dashboards)/dashboard/admin/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function MainAdminDashboardPage() {
+  return <div>MainAdminDashboardPage</div>;
+}
+
+export default MainAdminDashboardPage;
---
+++ app/(dashboards)/dashboard/user/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function MainUserDashboardPage() {
+  return <div>MainUserDashboardPage</div>;
+}
+
+export default MainUserDashboardPage;
