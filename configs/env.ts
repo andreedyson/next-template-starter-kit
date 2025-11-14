79da9fbdacbdcb081fcd887764085eacef0e3b35
Dinann <notxurd46@gmail.com>
2025-11-14 22:02:17 +0700

    refactor(prisma): remove unused migration files and clean up schema

--- configs/env.ts
+++ configs/env.ts
@@ -1,15 +1,8 @@
 import { z } from "zod";
 
 const envSchema = z.object({
-  NODE_ENV: z
-    .enum(["development", "production", "test"])
-    .default("development"),
-  DATABASE_URL: z.string({ message: "DATABASE URL is required" }),
-  DIRECT_URL: z.string().optional(), // If you use Supabase
+  NODE_ENV: z.enum(["DEVELOPMENT", "PRODUCTION"]).default("DEVELOPMENT"),
   NEXT_PUBLIC_API_URL: z.string(),
-  NEXTAUTH_SECRET: z
-    .string()
-    .min(10, "NEXTAUTH_SECRET must be at least 10 characters"),
   // Add other environment variables
 });
 
--- prisma/migrations/20250405134606_init/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateEnum
-CREATE TYPE "SystemRole" AS ENUM ('SUPER_ADMIN', 'ADMIN', 'USER');
-
--- AlterTable
-ALTER TABLE "User" ALTER COLUMN "role" SET DEFAULT 'USER';
